b1831a4bdbbaa291f8b534587cce14b64f787b09
veselinn16 <veselinn16@gmail.com>
2018-09-12 13:50:19 +0300

    Add List model :wrench:

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // Global app controller
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
@@ -113,5 +114,6 @@ elements.recipe.addEventListener('click', e => {
         state.recipe.updateServings('increase');
         recipeView.updateServingsIngredients(state.recipe);
     }
-    console.log(state.recipe)
 })
+
+window.l = new List();
